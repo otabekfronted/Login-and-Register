173f5bbccd79c65eb611bccaf8e59bbf18007747
Otabekfrontend <otabekmadaminov1022@gmail.com>
2024-07-01 21:22:06 +0500

    add navbar and pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <>
+            <div className="navbar bg-base-100 pl-8 pr-8 bg-slate-400">
+                <div className="flex-1">
+                    <Link to="/" className="btn btn-ghost text-xl">
+                        daisyUI
+                    </Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li>
+                            <details>
+                                <summary className="text-xl">Pages</summary>
+                                <ul className="bg-base-100 rounded-t-none p-2">
+                                    <li>
+                                        <Link to="/about">About</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/contact">Contact</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/login">Login</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/register">Register</Link>
+                                    </li>
+                                </ul>
+                            </details>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 function Home() {
-    return <div>Home</div>;
+    return (
+        <div className="text-center font-semibold text-4xl mt-14">
+            <h1>Hello Home Page</h1>
+        </div>
+    );
 }
 
 export default Home;
