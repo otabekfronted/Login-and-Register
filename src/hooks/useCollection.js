7808a4e474f5d5c6ec6b031cae7168f0e483fc2b
Otabekfrontend <otabekmadaminov1022@gmail.com>
2024-07-13 07:27:26 +0500

    add new function

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,26 +1,33 @@
-// firbase
+//firebase
 import { db } from "../firebase/firebaseConfig";
-
-import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
-
-// react
+import {
+    collection,
+    query,
+    onSnapshot,
+    orderBy,
+    where,
+} from "firebase/firestore";
+//react
 import { useEffect, useState } from "react";
 
-export const useCollection = (collectionName, orderName) => {
+export const useCollection = (collectionName, whereName, orderName) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        const q = query(collection(db, collectionName), orderBy(orderName));
+        const q = query(
+            collection(db, collectionName),
+            where(...whereName),
+            orderBy(...orderName)
+        );
+
         onSnapshot(q, (querySnapshot) => {
             const todos = [];
             querySnapshot.docs.forEach((doc) => {
-                todos.push({
-                    id: doc.id,
-                    ...doc.data(),
-                });
+                todos.push({ id: doc.id, ...doc.data() });
             });
             setData(todos);
         });
     }, [collectionName]);
+
     return { data };
 };
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,39 +1,79 @@
-// custom hooks
-import { useEffect } from "react";
-import { FormInput } from "../components";
-import { useCollection } from "../hooks/useCollection";
-
-// rrd imports
+//rrd import
 import { Form, useActionData } from "react-router-dom";
-
-// firbase
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+//custom hooks
+import { useCollection } from "../hooks/useCollection";
+//components
+import { FormInput } from "../components";
+import { useEffect } from "react";
+//firebase
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    doc,
+    serverTimestamp,
+    updateDoc,
+} from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import toast from "react-hot-toast";
 
-// action
+//context
+import { useGlobalContext } from "../hooks/useGlobalContext";
+
+//action
 export const action = async ({ request }) => {
     let formData = await request.formData();
-
     let title = formData.get("title");
     return { title };
 };
 
-export function Todos() {
-    const { data } = useCollection("todos");
+function TodoList() {
+    const { user } = useGlobalContext();
+
+    const { data } = useCollection(
+        "todos",
+        ["uid", "==", user.uid],
+        ["createdAt"]
+    );
+
     const dataTodo = useActionData();
+
+    const handleDelete = (id) => {
+        deleteDoc(doc(db, "todos", id))
+            .then(() => {
+                toast.success("Deleted todo.");
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            });
+    };
+
+    const handleCompleted = (id, status) => {
+        const todoRef = doc(db, "todos", id);
+
+        updateDoc(todoRef, {
+            completed: !status,
+        })
+            .then(() => {
+                toast.success("Updated !");
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            });
+    };
 
     useEffect(() => {
         if (dataTodo) {
             const newTodo = {
                 ...dataTodo,
-                completed: true,
-                createdAt: serverTimestamp,
+                comleted: true,
+                createdAt: serverTimestamp(),
+                uid: user.uid,
             };
 
             addDoc(collection(db, "todos"), newTodo)
                 .then(() => {
-                    toast.success("New Todo Added");
+                    toast.success("New todo added!");
                 })
                 .catch((error) => {
                     toast.error(error.message);
@@ -42,23 +82,53 @@ export function Todos() {
     }, [dataTodo]);
 
     return (
-        <div className="align-elemnts mt-10">
+        <div>
             {data &&
                 data.map((todo) => {
                     return (
-                        <div key={todo.id}>
-                            <h3 className="text-3xl font-bold">{todo.title}</h3>
+                        <div key={todo.id} className="flex flex-col w-full">
+                            <div
+                                className={`${
+                                    todo.completed
+                                        ? "opacity-35 "
+                                        : "opacity-100"
+                                }`}
+                            >
+                                <h2 className="pl-5 text-3xl">{todo.title}</h2>
+                            </div>
+                            <div className=" gap-4">
+                                <button
+                                    onClick={() =>
+                                        handleCompleted(todo.id, todo.completed)
+                                    }
+                                    className="btn btn-warning px-14 my-5"
+                                >
+                                    Completed
+                                </button>
+                                <button
+                                    onClick={() => handleDelete(todo.id)}
+                                    className="btn btn-warning px-14 my-5"
+                                >
+                                    Delete
+                                </button>
+                            </div>
                         </div>
                     );
                 })}
+
             <div>
-                <Form method="post" className="flex flex-col gap-5">
-                    <FormInput name="title" label="Title" type="text" />
-                    <button className="btn btn-secondary">Add</button>
+                <Form method="post">
+                    <FormInput
+                        name="title"
+                        label="Title :"
+                        type="text"
+                        placeholder="Creat new Todo"
+                    />
+                    <button className="btn btn-info btn-block">Add</button>
                 </Form>
             </div>
         </div>
     );
 }
 
-export default Todos;
+export default TodoList;
